06e246066ec52ed5377c5a65c0633c2a9822e2e4
Paul Molodowitch <elrond79@gmail.com>
2026-08-07 17:21:01 -0700

    Fix incorrect SSL_CERT_FILE path for windows + git-bash
    https://github.com/AnacondaRecipes/openssl-feedstock/commit/23e4d141 fixed an
    issue where a typo used a env var called __CONDA_OPENSLL_CERT_FILE_SET (SLL)
    instead of __CONDA_OPENSSL_CERT_FILE_SET (SSL).
    
    However, in activate.sh, it also DROPPED the Library subdir for SSL_CERT_FILE,
    going from
    
    export SSL_CERT_FILE="${CONDA_PREFIX}\\Library\ssl\\cacert.pem"
    
    to
    
    export SSL_CERT_FILE="${CONDA_PREFIX}/ssl/cacert.pem"
    
    Since it also switched from backward to forward slashes, it seems the author
    of the change assumed that since the script was a .sh, it was targeting Linux
    systems, and went with the Linux path, and Linux slashes.
    
    However, this activate.sh is ONLY copied into the activate.d directory for an
    env by bld.bat, and NOT by bld.sh - meaning that this is actually a windows-only
    file (for use by, ie, git-bash)... meaning that the old path is correct.
    
    This resulted in a broken SSL_CERT_FILE, causing errors in git-bash on
    windows when using uv, among others.
    
    This PR restores the old, windows-specific path.

--- recipe/activate.sh
+++ recipe/activate.sh
@@ -1,4 +1,4 @@
 if [[ "${SSL_CERT_FILE:-}" == "" ]]; then
-    export SSL_CERT_FILE="${CONDA_PREFIX}/ssl/cacert.pem"
+    export SSL_CERT_FILE="${CONDA_PREFIX}\\Library\ssl\\cacert.pem"
     export __CONDA_OPENSSL_CERT_FILE_SET="1"
 fi
